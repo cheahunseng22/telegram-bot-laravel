5722e5e3c9e1ee5376315258a972f65a12902648
cheahunseng22 <cheahun2016@gmail.com>
2026-03-16 20:36:37 +0700

    Update TelegramController.php

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -16,42 +16,73 @@ class TelegramController extends Controller
             return response()->json(['ok'=>true]);
         }
 
-        // MENU
         $menu = [
             '1' => ['Burger',5],
             '2' => ['Pizza',7],
             '3' => ['Coffee',3],
         ];
 
-        // START
+        /* START */
         if ($text == '/start') {
 
             Telegram::sendMessage([
                 'chat_id'=>$chat,
-                'text'=>"🙏 Welcome to E-Manu Food 🍽\nFounded by Cheahun\n\nMenu:\n1 Burger \$5\n2 Pizza \$7\n3 Coffee \$3\n\nType number to order."
+                'text'=>"🙏 Welcome to E-Manu Food 🍽\nFounded by Cheahun\n\nChoose language:\n1 Khmer 🇰🇭\n2 English 🇬🇧"
             ]);
 
         }
 
-        // ORDER
+        /* KHMER MENU */
+        elseif ($text == '1') {
+
+            Telegram::sendMessage([
+                'chat_id'=>$chat,
+                'text'=>"🍔 មឺនុយ\n1 បឺហ្គឺ \$5\n2 ពីហ្សា \$7\n3 កាហ្វេ \$3\n\nសូមវាយលេខដើម្បីបញ្ជាទិញ"
+            ]);
+
+        }
+
+        /* ENGLISH MENU */
+        elseif ($text == '2') {
+
+            Telegram::sendMessage([
+                'chat_id'=>$chat,
+                'text'=>"🍔 Menu\n1 Burger - \$5\n2 Pizza - \$7\n3 Coffee - \$3\n\nType number to order."
+            ]);
+
+        }
+
+        /* ORDER */
         elseif (isset($menu[$text])) {
 
             $item = $menu[$text][0];
             $price = $menu[$text][1];
 
+            $qr = "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=PAY-$price";
+
             Telegram::sendMessage([
                 'chat_id'=>$chat,
-                'text'=>"You ordered: $item\nPrice: $$price\n\nType /start to order again."
+                'text'=>"🧾 Order: $item\nPrice: $$price\n\nScan this Fake QR to pay:\n$qr\n\nAfter payment type: paid"
             ]);
 
         }
 
-        // UNKNOWN
+        /* PAYMENT CONFIRM */
+        elseif ($text == 'paid') {
+
+            Telegram::sendMessage([
+                'chat_id'=>$chat,
+                'text'=>"✅ Payment received (Fake)\n\nThank you for ordering 🙏\n\nChoose language again:\n1 Khmer 🇰🇭\n2 English 🇬🇧"
+            ]);
+
+        }
+
+        /* UNKNOWN */
         else {
 
             Telegram::sendMessage([
                 'chat_id'=>$chat,
-                'text'=>"Type /start to see menu."
+                'text'=>"Type /start to begin ordering 🍽"
             ]);
 
         }
