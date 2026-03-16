520c84eeb7573e350dbb7b806729ba85a8b0d6d4
cheahunseng22 <cheahun2016@gmail.com>
2026-03-16 20:19:27 +0700

    Update TelegramController.php

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Telegram\Bot\Laravel\Facades\Telegram;
-use Illuminate\Support\Facades\Cache;
 
 class TelegramController extends Controller
 {
@@ -13,93 +12,50 @@ class TelegramController extends Controller
         $chat = $request['message']['chat']['id'] ?? null;
         $text = strtolower(trim($request['message']['text'] ?? ''));
 
-        if (!$chat) return response()->json(['ok'=>true]);
+        if (!$chat) {
+            return response()->json(['ok'=>true]);
+        }
 
-        $user = Cache::get("user_$chat", [
-            'state'=>'start',
-            'lang'=>null
-        ]);
+        // MENU
+        $menu = [
+            '1' => ['Burger',5],
+            '2' => ['Pizza',7],
+            '3' => ['Coffee',3],
+        ];
 
-        if ($user['state']=='start') {
+        // START
+        if ($text == '/start') {
 
             Telegram::sendMessage([
                 'chat_id'=>$chat,
-                'text'=>"🙏 Welcome to E-Manu Food 🍽\nFounded by Cheahun\n\nChoose language:\n1 Khmer\n2 English"
+                'text'=>"🙏 Welcome to E-Manu Food 🍽\nFounded by Cheahun\n\nMenu:\n1 Burger \$5\n2 Pizza \$7\n3 Coffee \$3\n\nType number to order."
             ]);
 
-            $user['state']='language';
+        }
+
+        // ORDER
+        elseif (isset($menu[$text])) {
+
+            $item = $menu[$text][0];
+            $price = $menu[$text][1];
+
+            Telegram::sendMessage([
+                'chat_id'=>$chat,
+                'text'=>"You ordered: $item\nPrice: $$price\n\nType /start to order again."
+            ]);
 
         }
 
-        elseif ($user['state']=='language') {
+        // UNKNOWN
+        else {
 
-            if ($text=='1') $user['lang']='kh';
-            elseif ($text=='2') $user['lang']='en';
-            else {
-                Telegram::sendMessage([
-                    'chat_id'=>$chat,
-                    'text'=>"Please type 1 or 2"
-                ]);
-                Cache::put("user_$chat",$user,3600);
-                return;
-            }
-
-            $user['state']='menu';
-            $this->menu($chat,$user['lang']);
-        }
-
-        elseif ($user['state']=='menu') {
-
-            $menu=[
-                1=>['Burger',5],
-                2=>['Pizza',7],
-                3=>['Coffee',3]
-            ];
-
-            if(isset($menu[$text])){
-
-                $item=$menu[$text][0];
-                $price=$menu[$text][1];
-
-                Telegram::sendMessage([
-                    'chat_id'=>$chat,
-                    'text'=>"You selected $item\nPrice: $$price\n\nType 'paid' after payment."
-                ]);
-
-                $user['state']='payment';
-            }
+            Telegram::sendMessage([
+                'chat_id'=>$chat,
+                'text'=>"Type /start to see menu."
+            ]);
 
         }
-
-        elseif ($user['state']=='payment') {
-
-            if($text=='paid'){
-
-                Telegram::sendMessage([
-                    'chat_id'=>$chat,
-                    'text'=>"✅ Payment received!\nThank you for ordering."
-                ]);
-
-                $user['state']='menu';
-                $this->menu($chat,$user['lang']);
-            }
-        }
-
-        Cache::put("user_$chat",$user,3600);
 
         return response()->json(['ok'=>true]);
-    }
-
-
-    private function menu($chat,$lang)
-    {
-        $text = $lang=='kh'
-        ? "🍔 មឺនុយ\n1 Burger \$5\n2 Pizza \$7\n3 Coffee \$3"
-        : "🍔 Menu\n1 Burger \$5\n2 Pizza \$7\n3 Coffee \$3";
-
-        Telegram::sendMessage([
-            'chat_id'=>$chat,
-            'text'=>$text
-        ]);
     }
 }
